6d367b0658ad90c5db3a2ddb436ab249d973b161
Frankie <26461046+frankie-tech@users.noreply.github.com>
2022-11-20 16:28:58 -0500

    Updates plugin to include a transform mode

--- .eleventy.js
+++ .eleventy.js
@@ -1,30 +1,45 @@
+// @ts-check
 // Example use:
 // {% plausible %}
-
-module.exports = (eleventyConfig, _options = {}) => {
+const defaults = {
+  domain: 'replace.me', // the bare domain that plausible is keeping track of
+  // Cannot remove defer attribute
+  // not recommended by the default plausible js snippet, 
+  // but here's the option anyways :)
+  async: false,
+  transform: false // add a transform
+};
+/**
+ * 
+ * @param {import('@11ty/eleventy/src/UserConfig')} eleventyConfig 
+ * @param {typeof defaults} _options 
+ */
+module.exports = (eleventyConfig, _options) => {
   // Define defaults for your plugin config
-  const defaults = {
-    domain: 'replace.me', // the bare domain that plausible is keeping track of
-    // Cannot remove defer attribute
-    // not recommended by the default plausible js snippet, 
-    // but here's the option anyways :)
-    async: false 
-  };
+  /**
+   * @type {typeof defaults}
+   */
+  const options = {
+    ...defaults,
+    ..._options
+  }
+  let { domain, async } = options;
+  if (domain.startsWith('https://')) domain = domain.slice(8);
+  if (domain.startsWith('http://')) domain = domain.slice(7);
+  const attr = async ? 'async defer' : 'defer';
+
+  const el = `<script ${attr} data-domain="${domain}" src="https://plausible.io/js/plausible.js"></script>`;
 
   // You can create more than filters as a plugin, but here's an example
-  eleventyConfig.addShortcode("plausible", () => {
-    const options = {
-      ...defaults,
-      ..._options
-    }
+  eleventyConfig.addShortcode("plausible", () => el);
 
-    let { domain, async } = options;
-
-    if (domain.startsWith('http://')) domain = domain.slice(7);
-    if (domain.startsWith('https://')) domain = domain.slice(8);
-
-    const attr = async ? 'async defer' : 'defer';
-
-    return `<script ${attr} data-domain="${domain}" src="https://plausible.io/js/plausible.js"></script>`;
-  });
+  // @ts-ignore
+  if (options.transform) {
+    eleventyConfig.addTransform("plausible", (content, outputPath) => {
+      if (outputPath && outputPath.endsWith('.html')) {
+        return content.split('</head>').join(`${el}</head>`);
+      }
+      return content;
+    })
+  }
 };
